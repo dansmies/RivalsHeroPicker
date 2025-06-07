eea1f90e10fd9b1fd9015630e04f175fac8303f5
dansmieszny <danielsmieszny@outlook.com>
2025-06-07 18:10:09 -0400

    fixing hela

--- RivalsHeroPicker/obj/Debug/net9.0/RivalsHeroPicker.AssemblyInfo.cs
+++ RivalsHeroPicker/obj/Debug/net9.0/RivalsHeroPicker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RivalsHeroPicker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82fb8460ba3f5ff25e586e0a65cb5c85fcfe2cf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("RivalsHeroPicker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RivalsHeroPicker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
